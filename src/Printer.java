b80d6c53a4e785091f31d77910e333bfd91c46b3
doctorkho <khonisasha@yandex.ru>
2016-06-02 10:20:21 +0000

    Add files via upload

--- src/Printer.java
+++ src/Printer.java
@@ -1,9 +1,21 @@
-/**
- * Created by user on 6/2/2016.
- */
-public class Printer {
-    public void display (int data){
-        System.out.print(data);
-    }
+import java.lang.*;
 
+public class Printer {
+    private int[] _saveData = new int[3];
+    public void display (int data) {
+        try {
+            if (_saveData.length == ObserverData.count) {
+                throw (new error());
+            }
+            _saveData[ObserverData.count] = data;
+            ObserverData.count++;
+            System.out.println(data);
+
+
+        }
+        catch (error err){
+            err.Message();
+        }
+
+    }
 }
---
+++ src/error.java
@@ -0,0 +1,6 @@
+public class error extends Throwable {
+    public String p;
+    public static void Message(){
+        System.out.println("Переполнение массива");
+    }
+}
